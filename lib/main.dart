d975128e6931b9a29ae46575c8a3775892b926c0
Diawara Karim <karimdiawara96@gmail.com>
2023-04-16 11:44:51 +0000

    add in main.dart the class of provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:new_bloc/stateManagement/Provider/Counter.provider.dart';
 import 'package:new_bloc/ui/pages/CounterPage/Counter.bloc.page.dart';
+import 'package:new_bloc/ui/pages/CounterPage/Counter.provider.page.dart';
 import 'package:new_bloc/ui/pages/HomePage.dart';
 import 'package:new_bloc/ui/pages/ProfilePage.dart';
 import 'package:new_bloc/ui/pages/SettingsPage.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+// -----------------------------MYAPP PRINCIPAL--------------------------------
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -35,6 +39,34 @@ class RootView extends StatelessWidget {
         '/setting': (context) => const SettingsPage(),
       },
       initialRoute: "/",
+    );
+  }
+}
+
+// -------------------------MyApp de Provider --------------------------------
+class ProviderNotified extends StatelessWidget {
+  const ProviderNotified({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ---------------------NOTIFIERPROVIDER------------------------------
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => CounterState(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          "/": (context) => HomeProvider(),
+        },
+        title: 'Counter app',
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+        ),
+      ),
     );
   }
 }
