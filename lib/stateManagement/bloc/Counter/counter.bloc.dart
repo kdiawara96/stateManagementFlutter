584dfae501dad7ea3023cc24b2576cf35a80f545
Diawara Karim <karimdiawara96@gmail.com>
2023-04-16 15:31:30 +0000

    end class bloc

--- lib/stateManagement/bloc/Counter/counter.bloc.dart
+++ lib/stateManagement/bloc/Counter/counter.bloc.dart
@@ -1,5 +1,56 @@
+// ACTION ou EVENT
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+
 abstract class CounterEvent {}
 
-class IncrementEvent extends CounterEvent {}
+class IncrementCounterEvent extends CounterEvent {}
 
-class DecrementEvent extends CounterEvent {}
+class DecrementCounterEvent extends CounterEvent {}
+
+// STATES
+
+abstract class CounterState {
+  final int counter;
+
+  CounterState({required this.counter});
+}
+
+class CounterSuccessState extends CounterState {
+  CounterSuccessState({required super.counter});
+}
+
+class CounterErrorState extends CounterState {
+  final String errorMessage;
+  CounterErrorState({required this.errorMessage, required super.counter});
+}
+
+// Un ETat initiall
+
+class CounterIntialState extends CounterState {
+  CounterIntialState() : super(counter: 0);
+}
+
+// BLOC
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterIntialState()) {
+    // INCREMENT COUNTEER EVENT
+    on<IncrementCounterEvent>((event, emit) {
+      if (state.counter < 10) {
+        emit(CounterSuccessState(counter: state.counter + 1));
+      } else {
+        emit(CounterErrorState(errorMessage: "Max 10", counter: state.counter));
+      }
+    });
+//  DECREMENT COUNTER EVENT
+    on<DecrementCounterEvent>((event, emit) {
+      if (state.counter > 0) {
+        emit(CounterSuccessState(counter: state.counter - 1));
+      } else {
+        emit(CounterErrorState(errorMessage: "Min 0", counter: state.counter));
+      }
+    });
+  }
+}
